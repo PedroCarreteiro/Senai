abf98e5334ff3785d58ad6f261ad27643a3345cb
Pedro Augusto Padovani Carreteiro <49300445880@>
2025-03-12 14:58:26 -0300

    Alteração 16

--- ListaPBE11-03/ex16.py
+++ ListaPBE11-03/ex16.py
@@ -1,10 +1,10 @@
 import math
 
 x1 = int(input("Digite a coordenada de x1: "))
-x2 = int(input("Digite a coordenada de x2: "))
 y1 = int(input("Digite a coordenada de y1: "))
+x2 = int(input("Digite a coordenada de x2: "))
 y2 = int(input("Digite a coordenada de y2: "))
 
-conta = math.sqrt((x1-x2)**2 + (y1-y2)**2)
+conta = math.sqrt((x2-x1)**2 + (y2-y1)**2)
 
 print(f"A distância é: {conta}")
