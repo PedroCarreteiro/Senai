4fde11a6bbe42a4c00545e1ae4ed409742e55810
Pedro Augusto Padovani Carreteiro <49300445880@>
2025-03-12 14:25:15 -0300

    Finalizado

--- ListaPBE11-03/ex14.py
+++ ListaPBE11-03/ex14.py
@@ -1,2 +1,8 @@
 a = float(input("Insira o valor de A: "))
 b = float(input("Insira o valor de B: "))
+
+print(f"O valor de A é {a} e o valor de B é {b}")
+
+a, b = b, a
+
+print(f"O valor de A trocado é {a} e o valor de B trocado é {b}")
