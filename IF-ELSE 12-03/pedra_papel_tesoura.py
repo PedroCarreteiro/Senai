10c8e48e0d1967337023feb6de161238661b8c49
Pedro Augusto Padovani Carreteiro <49300445880@>
2025-03-19 13:38:04 -0300

    Alteração Tesoura

--- IF-ELSE 12-03/pedra_papel_tesoura.py
+++ IF-ELSE 12-03/pedra_papel_tesoura.py
@@ -8,15 +8,21 @@ escolha = random.choice(opcoes)
 if chute == "P" or "A" or "T":
     if chute == escolha:
         print("Empate")
+        print(f"A escolha do computador é {escolha}")
     elif chute == "P" and escolha == "A":
         print("Você perdeu")
+        print(f"A escolha do computador é {escolha}")
     elif chute == "P" and escolha == "T":
         print("Você ganhou!")
+        print(f"A escolha do computador é {escolha}")
     elif chute == "A" and escolha == "T":
         print("Você perdeu")
+        print(f"A escolha do computador é {escolha}")
     elif chute == "T" and escolha == "P":
         print("Você perdeu")
+        print(f"A escolha do computador é {escolha}")
     elif chute == "T" and escolha == "A":
         print("Você ganhou")
+        print(f"A escolha do computador é {escolha}")
 else:
     print("Opção inválida")
