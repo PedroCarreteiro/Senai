03cd20227993aadbd4f755b6436cdf0e33f9bc47
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-01 14:15:02 -0300

    Update ex5.py

--- ListaPBE25-03/ex5.py
+++ ListaPBE25-03/ex5.py
@@ -2,5 +2,7 @@ numero = int(input("Digite um numero: "))
 
 if numero >= 1:
     print("Positivo")
+elif numero == 0:
+    print("Número é 0")
 else:
-    print("Menor de idade")
+    print("Negativo")
