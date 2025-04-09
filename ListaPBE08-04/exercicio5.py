27617303c74624f54a0a4629f4234ed42a6a1569
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-09 14:45:22 -0300

    Update exercicio5.py

--- ListaPBE08-04/exercicio5.py
+++ ListaPBE08-04/exercicio5.py
@@ -1,10 +1,13 @@
-numero = int(input("Digite um número positivo: "))
-if numero > 1:
-    for i in range(2, numero): #testar números do 2 até o número - 1
-        if numero % i == 0:
-            print(f"O número {numero} não é primo.")
-            break
-    else:
-        print(f"O número {numero} é primo.")
+numero = int(input("Digite um número: (ele dever ser positivo): "))
+if numero == 1:
+    print(f"O número {numero} não é primo.")
 else:
-    print("INVALIDO")
+    if numero > 1:
+        for i in range(2, numero): #testar números do 2 até o número - 1
+            if numero % i == 0:
+                print(f"O número {numero} não é primo.")
+                break
+        else:
+            print(f"O número {numero} é primo.")
+    else:
+        print("INVALIDO")
