f76288aeb6f803bd7452e3204f0c764a63801823
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-09 20:18:46 -0300

    Update exercicio9.py

--- WhileTrue09-04/exercicio9.py
+++ WhileTrue09-04/exercicio9.py
@@ -6,10 +6,11 @@ for i in range(lista_tamanho):
     i += 1
     numero = float(input(f"Digite o número {i}: "))
     lista.insert(1, numero)
-    soma = sum(lista)
-    media = soma / lista_tamanho
-    for n in range(lista_tamanho):
-        if lista[n] < media:
-            lista_menores.append(numero)
+soma = sum(lista)
+media = soma / lista_tamanho
+
+for n in range(lista_tamanho):
+    if lista[n] < media:
+        lista_menores.append(lista[n])
 
 print(f"Os números abaixo da média são: {lista_menores}")
