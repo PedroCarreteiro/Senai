31c75e6eb190399775041805129c82ae9c496e28
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-10 09:15:36 -0300

    Update desafio2.py

--- WhileTrue09-04/desafio2.py
+++ WhileTrue09-04/desafio2.py
@@ -3,4 +3,9 @@ import random
 lista_palavras = ["Abacaxi","Banana","Uva","Jabuticaba","Maracujá","Morango","Limão","Laranja"]
 
 palavra_aleatoria = random.choice(lista_palavras)
+lista_palavra_aleatoria = [char for char in palavra_aleatoria]
 
+palavra_crip = ['_' * len(lista_palavra_aleatoria)]
+
+print(lista_palavra_aleatoria)
+print(palavra_crip)
