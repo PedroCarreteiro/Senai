7d4295ca3e1cdbff131636f60c31db0bb7c91642
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-11 08:09:11 -0300

    Update exercicio9.py

--- ListaPBE08-04/exercicio9.py
+++ ListaPBE08-04/exercicio9.py
@@ -1,15 +1,15 @@
 numero = 0
 lista_numeros = []
 
-for numero in range(1, 10000):
-    lista_contador = []
+for numero in range(1, 10000): #for para numero de 1 até 10000
+    lista_contador = [] #zerar a lista de contador
 
-    for i in range(1, numero):
-        if numero % i == 0:
-            lista_contador.append(i)
+    for i in range(1, numero): #for para contador de 1 até numero
+        if numero % i == 0: #se numero / contador der resto 0
+            lista_contador.append(i) #adicione contador na lista de contador
 
-    soma = sum(lista_contador)
-    if soma == numero:
-        lista_numeros.append(numero)
+    soma = sum(lista_contador) #somar lista de contador
+    if soma == numero: #se a soma de todos os divisores de numero for igual ao numero
+        lista_numeros.append(numero) #adicionar numero na lista de perfeitos
 
 print(f"A lista de números perfeitos de 1 até 10000 é {lista_numeros}")
