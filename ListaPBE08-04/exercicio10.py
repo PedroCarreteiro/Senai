3314b9f5ca82451d6409675b9e5a7c130e231322
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-04-12 17:09:35 -0300

    Update exercicio10.py

--- ListaPBE08-04/exercicio10.py
+++ ListaPBE08-04/exercicio10.py
@@ -1,44 +1,44 @@
 import math
-comeco = int(input("Digite um número para começar a verificação: "))
-final = int(input("Digite um número para finalizar a verificação: "))
+from os import remove
 
-str_esquerda = ""
-str_direita = ""
-int_esquerda = 0
-int_direita = 0
-kaprekar = []
-quadrado = 0
+numero_inicio = int(input("Digite o número de início: "))
+numero_fim = int(input("Digite o número de fim: "))
 
-if comeco < 0 or final < 0:
+numero_quadrado = 0
+numero_quadrado_esquerda = 0
+numero_quadrado_direita = 0
+numeros_kaprekar = []
+
+if numero_inicio < 0 or numero_fim < 0:
     print("INVALIDO")
-elif comeco > final:
+elif numero_inicio > numero_fim:
     print("INVALIDO")
 else:
-    for numero in range (comeco, final+1):
-        str_esquerda = ""
-        str_direita = ""
-        quadrado = int(quadrado)
-        quadrado = 0
+    for numero in range (numero_inicio, numero_fim+1):
+        numero_quadrado_string_esquerda = ""
+        numero_quadrado_string_direita = ""
+        numero_quadrado = int(numero_quadrado)
+        numero_quadrado = 0
 
-        quadrado = numero**2
-        quadrado = str(quadrado)
+        numero_quadrado = numero**2
+        numero_quadrado = str(numero_quadrado)
 
-        meio = len(quadrado) / 2
-        meio = math.floor(meio)
+        numero_meio = len(numero_quadrado) / 2 #achar o meio da lista
+        numero_meio = math.floor(numero_meio) #arredondar para baixo
 
-        str_esquerda = quadrado[0 : meio]
-        str_direita = quadrado[meio : len(quadrado)]
+        numero_quadrado_string_esquerda = numero_quadrado[0 : numero_meio] #pegar a esquerda do numero ao quadrado
+        numero_quadrado_string_direita = numero_quadrado[numero_meio : len(numero_quadrado)] #pegar a direita
 
-        if str_esquerda != '':    
-            int_esquerda = int(str_esquerda)
-        if str_direita != '':
-            int_direita = int(str_direita)
+        if numero_quadrado_string_esquerda != '':
+            numero_quadrado_esquerda = int(numero_quadrado_string_esquerda) #caso não seja vazio, transformar esquerda em int
+        if numero_quadrado_string_direita != '':
+            numero_quadrado_direita = int(numero_quadrado_string_direita) #caso não seja vazio, transformar direita em int
 
-        soma = int_direita + int_esquerda
+        soma = numero_quadrado_esquerda + numero_quadrado_direita
 
         if soma == numero:
-            kaprekar.append(numero)
+            numeros_kaprekar.append(numero)
 
-    kaprekar.remove(0)
-    print(kaprekar)  
- 
+    if 0 in numeros_kaprekar:
+        numeros_kaprekar.remove(0)
+    print(numeros_kaprekar)
