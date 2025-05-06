0a6291686c7e9082cbf6d6e1567f8dcc16b6e241
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-05-06 19:32:48 -0300

    atualizar ex12

--- Lista06-05/ex12.py
+++ Lista06-05/ex12.py
@@ -1,7 +1,49 @@
-n = 6
-r = 1
-lista = []
-for i in range(1,n+1):
-    r *= i
+n1 = 3
+r1 = 1
 
-print(r)
+if n1 > 0:
+    for i in range(1,n1+1):
+        r1 *= i
+    print(f"O fatorial de {n1} é {r1}")
+else:
+    print(f"O número {n1} é negativo, portanto não pode ser utilizado em fatoriais")
+
+n2 = 7
+r2 = 1
+
+if n2 > 0:
+    for i in range(1,n2+1):
+        r2 *= i
+    print(f"O fatorial de {n2} é {r2}")
+else:
+    print(f"O número {n2} é negativo, portanto não pode ser utilizado em fatoriais")
+
+n3 = 9
+r3 = 1
+
+if n3 > 0:
+    for i in range(1,n3+1):
+        r3 *= i
+    print(f"O fatorial de {n3} é {r3}")
+else:
+    print(f"O número {n3} é negativo, portanto não pode ser utilizado em fatoriais")
+
+n4 = 25
+r4 = 1
+
+if n4 > 0:
+    for i in range(1,n4+2):
+        r4 *= i
+    print(f"O fatorial de {n4} é {r4}")
+else:
+    print(f"O número {n4} é negativo, portanto não pode ser utilizado em fatoriais")
+
+n5 = 26
+r5 = 1
+
+if n5 > 0:
+    for i in range(1,n5+2):
+        r5 *= i
+    print(f"O fatorial de {n5} é {r5}")
+else:
+    print(f"O número {n5} é negativo, portanto não pode ser utilizado em fatoriais")
