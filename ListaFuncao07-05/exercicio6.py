d158fbc384c1780fa49d5da1b678ecbbb725fdbd
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-05-07 21:47:57 -0300

    atualizar exercicio6.py

--- ListaFuncao07-05/exercicio6.py
+++ ListaFuncao07-05/exercicio6.py
@@ -1,12 +1,12 @@
 def contar_vogais(palavra):
     contador = 0
+    palavra = palavra.lower()
     palavra = list(palavra)
-    print(palavra)
     for i in range(len(palavra)):
-        if palavra[i] == "a":
+        if palavra[i] == "a" or palavra[i] == "e" or palavra[i] == "i" or palavra[i] == "o" or palavra[i] == "u":
             contador += 1
     return contador
 
 palavra = input("Digite uma palavra: ")
 
-print(f"Essa palavra tem: {contar_vogais(palavra)} vogais")
+print(f"A palavra {palavra} tem: {contar_vogais(palavra)} vogais")
