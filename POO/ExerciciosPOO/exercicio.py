70230759e2a96001e7fcd0ae79922a529d0dbe87
Pedro Carreteiro <140073394+PedroCarreteiro@users.noreply.github.com>
2025-06-10 19:42:50 -0300

    Update exercicio.py

--- POO/ExerciciosPOO/exercicio.py
+++ POO/ExerciciosPOO/exercicio.py
@@ -8,32 +8,32 @@ class ItemBiblioteca:
         else:
             self.ano_publicacao = ano_publicacao
 
-        def emprestar(self):
-            if self.disponivel:
-                self.disponivel = False
-                print("Livro emprestado")
-            else:
-                print("Este livro já foi emprestado!")
+    def emprestar(self):
+        if self.disponivel:
+            self.disponivel = False
+            print("Livro emprestado")
+        else:
+            print("Este livro já foi emprestado!")
 
-        def devolver(self):
-            if self.disponivel:
-                print("Este livro já está disponível!")
-            else:
-                self.disponivel = True
-                print("Livro devolvido!")
+    def devolver(self):
+        if self.disponivel:
+            print("Este livro já está disponível!")
+        else:
+            self.disponivel = True
+            print("Livro devolvido!")
             
-        def obter_info(self):
-            return {
-                "nome": self.nome,
-                "ano_publicacao": self.ano_publicacao,
-                "disponivel": self.disponivel
-            }
-            print(f"Título: {self.nome}\n")
-            print(f"Ano: {self.ano_publicacao}\n")
-            if disponivel:
-                print("Disponível: Sim\n")
-            else:
-                print("Disponível: Não")
+    def obter_info(self):
+        # return {
+        #     "titulo": self.titulo,
+        #     "ano_publicacao": self.ano_publicacao,
+        #     "disponivel": self.disponivel
+        # }
+        print(f"\nTítulo: {self.titulo}")
+        print(f"Ano: {self.ano_publicacao}")
+        if self.disponivel:
+            print("Disponível: Sim")
+        else:
+            print("Disponível: Não")
 
 class ColecaoLivros(ItemBiblioteca):
     def __init__(self, titulo: str, ano_publicacao: int, disponivel: bool = True):
@@ -44,29 +44,36 @@ class ColecaoLivros(ItemBiblioteca):
         self.lista_livros.append(livro)
 
     def verificar_disponibilidade_colecao(self):
-        #verifcar se há algum itemm indisponível
+        #verifcar se há algum item indisponível
         for livro in self.lista_livros:
             if not livro.disponivel:
                 return False 
         return True
 
-    def obter_info(lista_livro):
-        super().obter_info()
-        for i in range (len(self.lista_livros)):
+    def obter_info(self):
+        super().obter_info()        
+        print(f"\nNome Coleção: {self.titulo}"
+              f"\nAno: {self.ano_publicacao}"
+              f"\nDisponível: {self.disponivel}"
+              f"\nLivros da coleção:")
+        
+        for i in range(len(self.lista_livros)):
             print(self.lista_livros[i].titulo)
 
 
-lista = [
-            ItemBiblioteca("livro1", 2004, True),
-            ItemBiblioteca("livro2", 2005, True),
-            ItemBiblioteca("livro3", 2043, False)
-        ]
+livro1 = ItemBiblioteca("livro1", 2004, True)
+livro2 = ItemBiblioteca("livro2", 2005, True)
+livro3 = ItemBiblioteca("livro3", 2043, False)
+
+livro1.emprestar()
+livro1.devolver()
+livro1.obter_info()
 
 
-lista.obter_info()
+colecao = ColecaoLivros("Colecao1", 2010, True)
+colecao.adicionar_livro(livro1)
+colecao.adicionar_livro(livro2)
+colecao.adicionar_livro(livro3)
 
-# for i in range (len(lista)):
-#     print(lista[i].titulo)
+colecao.obter_info()
 
-# colecao = ColecaoLivros("Colecao", 7777, True)
-# colecao.adicionar_livro("livro1", 2004, True)
